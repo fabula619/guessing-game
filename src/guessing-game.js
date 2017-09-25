3b673e9cb6f027789cf95e10a03fc09fbaa168bf
fabula619 <fabula619@gmail.com>
2017-09-25 22:57:59 +0300

    guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
-class GuessingGame {
+class GuessingGame
+{
     constructor() {}
 
-    setRange(min, max) {
-
+    setRange( min,  max) {
+    this.min=min;
+    this.max=max;
     }
 
     guess() {
 
+        return Math.ceil( (this.max+this.min)/2 );
     }
 
     lower() {
+        if(Math.ceil((this.max+this.min)/2) - (this.min+this.max)/2 > 0)
+            this.max = parseInt((this.max+this.min)/2 )+1;
+        else
+            this.max = parseInt((this.max+this.min)/2 );
 
     }
 
     greater() {
+        if(Math.ceil((this.max+this.min)/2) - (this.min+this.max)/2 > 0)
+            this.min = parseInt((this.max + this.min) / 2)+1;
+        else
+            this.min = parseInt((this.max+this.min)/2 );
 
     }
 }
-
 module.exports = GuessingGame;
